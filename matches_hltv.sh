ca62a53572720b56f7504e91bd3799da83046c8f
Jonas Toth <jonas.toth@gmail.com>
2015-05-02 14:18:22 +0200

    Fixed if-issue, croprange adjusted again

--- matches_hltv.sh
+++ matches_hltv.sh
@@ -72,7 +72,7 @@ do
 	echo "$raw_html" > "$tmp_file"
 
 	# sed read the tmp_file and crop of all unneeded stuff
-	crop=$(sed -n -e '555,902p' < "$tmp_file")
+	crop=$(sed -n -e '617,962p' < "$tmp_file")
 
 	# get every not needed div out of there
 	# erase all tabulators
@@ -139,7 +139,7 @@ do
 		map=${map%% *}
 		
 		# is there real data?
-		if [ -z $day ]
+		if [ -z "$day" ]
 		then
 			continue
 		fi
